37c153096663b215cf17d087da0e47f686d5fc36
Brandixitor <med.salah.bourouba@gmail.com>
2021-04-25 02:32:59 +0100

    Update

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -10,6 +10,7 @@ public class CharacterMovement : MonoBehaviour
 
     private void Update()
     {
+        //Basic left and right movement.
         var movement = Input.GetAxis("Horizontal");
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
     }
