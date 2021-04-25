75b68596257187463edf93ea62c9f7da66dddc0c
Brandixitor <med.salah.bourouba@gmail.com>
2021-04-25 02:36:03 +0100

    Update

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -3,6 +3,7 @@
 public class CharacterMovement : MonoBehaviour
 {
     public float MovementSpeed;
+    private bool facingRight;
     private void Start()
     {
         
@@ -13,6 +14,12 @@ public class CharacterMovement : MonoBehaviour
         //Basic left and right movement.
         var movement = Input.GetAxis("Horizontal");
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+    }
+
+    private void Flip()
+    {
+        //Character left and right 180 flips.
+        
     }
 }
 
